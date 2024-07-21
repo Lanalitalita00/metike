0c01b10d1c884dfb41cae20770cbd0650bdcc8ee
Lanalitalita00 <lanalitalita00@gmail.com>
2024-07-21 15:38:14 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Metike',
+  webDir: 'dist'
+};
+
+export default config;
