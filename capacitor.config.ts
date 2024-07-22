b168622dc59523538ac4a26be676f8be0e596488
lanalitalita00 <lanalitalita00@gmail.com>
2024-07-22 14:05:27 +0200

    Metike project without ionic

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Metike',
-  webDir: 'dist'
-};
-
-export default config;
